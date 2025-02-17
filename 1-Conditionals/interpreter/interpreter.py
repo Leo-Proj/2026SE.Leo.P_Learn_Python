0dfbb72c6302c007f961bb2bc84c04562f1aa79b
Leo P <leo.projceski@education.nsw.gov.au>
2025-02-17 00:35:09 +0000

    Upload completed interpreter.py

--- 1-Conditionals/interpreter/interpreter.py
+++ 1-Conditionals/interpreter/interpreter.py
@@ -1,15 +1,16 @@
-x, y, z = int(input("Question? ")).split()
+x, y, z = input("Question? ").split()
 
 x = int(x)
 z = int(z)
 
 if y == "+":
-    result = (x + y)
+    result = (x + z)
 elif y == "-":
-    result = (x - y)
+    result = (x - z)
 elif y == "x" or y == "*":
-    result = (x * y)
+    result = (x * z)
 elif result == "/":
-    y == (x / y)
+    y == (x / z)
 else:
     print("Math Error")
+print(f"{result:.1f}")
