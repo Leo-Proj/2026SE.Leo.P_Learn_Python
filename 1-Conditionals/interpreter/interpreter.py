93539cecb67567995dd06ad6ce2440fa8d3cd97c
Leo P <leo.projceski@education.nsw.gov.au>
2025-02-17 00:20:20 +0000

    interpreter code

--- 1-Conditionals/interpreter/interpreter.py
+++ 1-Conditionals/interpreter/interpreter.py
@@ -1,1 +1,15 @@
-x = int(input("Expression? "))
+x, y, z = int(input("Question? ")).split()
+
+x = int(x)
+z = int(z)
+
+if y == "+":
+    result = (x + y)
+elif y == "-":
+    result = (x - y)
+elif y == "x" or y == "*":
+    result = (x * y)
+elif result == "/":
+    y == (x / y)
+else:
+    print("Math Error")
