672207490eacca4057e9b2def369b95c9c5c59ad
Leo P <leo.projceski@education.nsw.gov.au>
2025-04-05 05:23:58 +0000

    upload completed fuel.py

---
+++ mondaychallenges.py
@@ -0,0 +1,15 @@
+List = ["Jeremy", "Harry", "Bart"]
+
+x = input("Index? ")
+
+print(List.index(x))
+
+#List_1 = ["Est", "Gro", "Mat"]
+#List_2 = ["Fat", "Son", "Hol"]
+
+#print("| Col 1 | Col 2 |")
+#print("| --- | --- |")
+
+#for i in range(len(List_1)):
+    #print(f"| {List_1[i]} | {List_2[i]} |")
+    #print("| --- | --- |")
