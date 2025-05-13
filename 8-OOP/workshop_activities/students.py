15b69b3d1519fc242d652c62ed36ed7464660c95
Leo P <leo.projceski@education.nsw.gov.au>
2025-05-13 11:28:37 +0000

    finished video

--- 8-OOP/workshop_activities/students.py
+++ 8-OOP/workshop_activities/students.py
@@ -1,15 +1,47 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+
 def main():
-    student = get_student()
-    if student[0] == "Padma":
-        student[1] = "Ravenclaw"
-    print(f"{student[0]} from {student[1]}")
+    student = Student.get()
+    print(student)
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return [name, house]
+    return Student(name, house)
+
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+#     def charm(self):
+        #match self.patronus:
+            #case "Stag":
+            #    return "🐴"
+            #case "Otter":
+           #     return "🦦"
+            #case "Jack Russell terrier":
+            #    return "🐶"
+           # case _:
+           #     return "🪄"
