b883b24115e0fca2d0961d2f8402dd4c33674348
Leo P <leo.projceski@education.nsw.gov.au>
2025-05-16 00:07:49 +0000

    completed problem 1 OOP

--- 8-OOP/seasons/seasons.py
+++ 8-OOP/seasons/seasons.py
@@ -1,12 +1,13 @@
 from datetime import date
-
+import sys
+import inflect
+p = inflect.engine()
 
 def main():
-	...
-
-
-...
-
+	birth_date = date.fromisoformat(input("Date of birth: "))
+	todays_date = date.today()
+	difference = (todays_date - birth_date).days
+	print(p.number_to_words(difference * 24 * 60) + " minutes")
 
 if __name__ == "__main__":
 	main()
