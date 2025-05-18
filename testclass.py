8a7112bef62e3f3d6e7f657ff220725dd397a06b
Leo P <leo.projceski@education.nsw.gov.au>
2025-05-18 08:21:39 +0000

    finished jar.py

---
+++ testclass.py
@@ -0,0 +1,8 @@
+class shopping:
+    def __init__(self, apple, banana, orange):
+        self.apple = apple
+        self.banana = banana
+        self.orange = orange
+
+bob = shopping(5, 7, 9)
+print(f"Bob has {bob.apple} apples.")
